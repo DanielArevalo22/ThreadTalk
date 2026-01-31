f03198cbf659b4519de4905e8b1bfee47e88cadc
DanielArevalo22 <josesant768@gmail.com>
2026-01-31 11:46:02 -0500

    INITIAL LOAD

---
+++ src/main/java/com/talk/threadtalk/ThreadTalk.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.talk.threadtalk;
+
+/**
+ *
+ * @author Daniel
+ */
+public class ThreadTalk {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
