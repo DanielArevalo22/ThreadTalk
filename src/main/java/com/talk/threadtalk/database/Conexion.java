fd4292c5ca6dac09e793dcf66d7e88ebc49fb850
DanielArevalo22 <josesant768@gmail.com>
2026-01-31 12:21:54 -0500

    CONNECTION CLASS CREATED

---
+++ src/main/java/com/talk/threadtalk/database/Conexion.java
@@ -0,0 +1,26 @@
+package com.talk.threadtalk.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    
+    private static final String URL = "jdbc:mysql://localhost:3306/threadTalk?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConexion() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al conectar con la base de datos", e);
+        }
+    }
+
+    public void closeConexion(Connection cn) throws SQLException{
+        if (cn!= null){
+            cn.close();
+        }
+    }
+}
