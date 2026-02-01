0e8a341c40d58b881ac735a77e9155f32585a8ae
DanielArevalo22 <josesant768@gmail.com>
2026-02-01 13:51:49 -0500

    MENSAJE EXCEPTION CREATED

---
+++ src/main/java/com/talk/threadtalk/Exceptions/MensajeException.java
@@ -0,0 +1,8 @@
+package com.talk.threadtalk.Exceptions;
+
+public class MensajeException extends RuntimeException {
+
+    public MensajeException(String mensaje) {
+        super(mensaje);
+    }
+}
